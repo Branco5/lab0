75163dbcc98373c56ab89421a602a6b0a863848a
Tiago Branco <72800800+Branco5@users.noreply.github.com>
2020-10-14 17:20:01 +0100

    teste3

---
+++ src/test/TestGroup.java
@@ -0,0 +1,15 @@
+package test;
+
+import com.pa.Person;
+import com.pa.Group;
+import org.junit.*;
+
+public class TestGroup{
+    Person person1 = new Person(1, "Tiago");
+    Person person2 = new Person(2, "Leo");
+    Person person3 = new Person(3, "Rui");
+
+    Group group1 = new Group(person1);
+    group1.addMember(person2);
+    group1.addMember(person3);
+}
--- src/test/TestPerson.java
+++ src/test/TestPerson.java
@@ -5,9 +5,10 @@ import org.junit.*;
 
 public class TestPerson {
 
+    Person p1 = new Person(1, "Joao");
+
     @Test
     public void testName(){
-        Person p1 = new Person(1, "Joao");
         Assert.assertEquals("Joao", p1.getName());
     }
 
